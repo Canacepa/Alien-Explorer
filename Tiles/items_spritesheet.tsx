78fbf8e3b231cdc64798efac452e292ce6585d7e
Canacepa <cassandre.lavallard@gmail.com>
2023-03-08 09:20:07 +0100

    -

---
+++ Tiles/items_spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="items_spritesheet" tilewidth="70" tileheight="70" spacing="2" tilecount="64" columns="8">
+ <image source="../Img/Base-pack/Items/items_spritesheet.png" width="576" height="576"/>
+</tileset>
